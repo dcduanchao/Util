aac69ee238d99963f1f713bf7f01d46ef6173fa7
duanchao <duanchao@zhidoushi.cn>
2019-08-08 11:10:30 +0800

    忽略

---
+++ java-stream/src/main/java/com/dc/javastream/JavaStreamApplication.java
@@ -0,0 +1,20 @@
+package com.dc.javastream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaStreamApplication {
+
+    public static void main(String[] args) {
+
+
+
+
+
+        //skald
+
+        SpringApplication.run(JavaStreamApplication.class, args);
+    }
+
+}
