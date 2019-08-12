0421bb2e41cbc776178d1763cb92a3bb11db833a
duanchao <duanchao@zhidoushi.cn>
2019-08-12 09:27:08 +0800

    注释

---
+++ sql-mongo-redis/src/main/java/com/dc/sqlmongoredis/SqlMongoRedisApplication.java
@@ -0,0 +1,13 @@
+package com.dc.sqlmongoredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlMongoRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqlMongoRedisApplication.class, args);
+    }
+
+}
---
+++ sql-mongo-redis/src/test/java/com/dc/sqlmongoredis/SqlMongoRedisApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dc.sqlmongoredis;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SqlMongoRedisApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
